46261f3dab2299e57b726e56fa06c92cd781d8c1
Veli Kalkan <108587243+hvklkn@users.noreply.github.com>
2024-08-28 10:38:42 +0300

    firstcommit

---
+++ ProductListWithCart/script/cart.js
@@ -0,0 +1,235 @@
+import { products } from "./products.js";
+import { cartQuantity, updateCartQuantity } from "./scripts.js";
+
+export let cart = [];
+
+export function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart')) || [];
+  //updateCartQuantity();
+}
+
+//func each time clicking +
+export function addToCart(productId, quantity = 1) {
+  //const product = products.find(item => item.id === productId);
+  //gotta add Number, to convert both of the number to the same datatype.
+  const product = products.find(item => item.id === Number(productId));
+  
+  if (product) {
+    const existingItem = cart.find(item => item.id === Number(productId));
+    
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      cart.push({ 
+        id: product.id, 
+        name: product.name, 
+        price: product.price, 
+        quantity 
+      });
+    }
+    saveToStorage();
+    updateCartQuantity();
+  }
+}
+
+//remove from cart func / clicking -
+export function removeFromCart(productId, quantity = 1) {
+  //gotta add Number, to convert both of the number to the same datatype.
+  const product = products.find(item => item.id === Number(productId));
+  
+  if (product) {
+    const existingItem = cart.find(item => item.id === Number(productId));
+    
+    if (existingItem) {
+      existingItem.quantity -= quantity;
+      if (existingItem.quantity <= 0) {
+        cart = cart.filter(item => item.id !== Number(productId));
+      }
+    }
+    saveToStorage();
+    updateCartQuantity();
+  }
+}
+
+//display the cart , after clicking add to cart/ adjust item quantity
+export function displayCart() {
+  let cartHTML = '';
+  let orderPrice = 0;
+  
+  cart.forEach((item) => {
+    const product = products.find(product => product.id === Number(item.id));
+    //only execute this if the product have at least 1 item.
+    if (item.quantity > 0) {
+      cartHTML += `
+        <div class="order" id="${item.id}">
+          <div class="cart-item">
+            <span class="item-name">${item.name}</span>
+            
+            <div class="item-attribute">
+              <div class="item-quantity js-item-quantity">
+                ${item.quantity}x
+              </div>
+              
+              <div class="item-price">
+                @\$${(product.price).toFixed(2)}
+              </div>
+              
+              <div class="total-price">
+                $${(product.price * item.quantity).toFixed(2)}
+              </div>
+            </div>
+          </div>
+
+          <button class="delete-item-button" data-product-id="${item.id}">
+            <img src="assets/images/icon-remove-item.svg">
+          </button>
+        </div>
+        <div class="seperator-line"></div>
+      `;
+    }
+    orderPrice += Number((product.price * item.quantity));
+  });
+
+  const cartWithItems = document.querySelector('.js-cart-with-items');
+  const cartEmpty = document.querySelector('.js-cart-empty');
+  
+  if (cartQuantity > 0) {
+    cartEmpty.style.display = "none";
+
+    cartHTML += `
+      <div class="order-price">
+        <span class="title">Order Total</span>
+        
+        <span>
+          <h2>$${orderPrice.toFixed(2)}</h2>
+        </span> 
+      </div>
+
+      <div class="carbon-neutral">
+        <img src="assets/images/icon-carbon-neutral.svg">
+        <span>This is a <b>carbon-neutral</b> delivery</span>
+      </div>
+
+      <div class="confirm-order-section">
+        <button class="confirm-order js-confirm-order">
+          Confirm Order
+        </button>
+      </div>
+    `;
+  } else {
+    cartEmpty.style.display = "block";
+    cartHTML = ``;
+  }
+  cartWithItems.innerHTML = cartHTML;
+  
+  // Attach event listener to the "Confirm Order" button
+  const itemQuantity = document.querySelector('.js-item-quantity');
+  const confirmOrderButton = document.querySelector('.js-confirm-order');
+  if (confirmOrderButton) {
+    confirmOrderButton.addEventListener('click', () => {
+      confirmOrder();
+    });
+  }
+
+  //delete buttons in cart: 
+  const deleteButtons = document.querySelectorAll('.delete-item-button');
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const productId = event.currentTarget.dataset.productId;
+      removeFromCart(productId, Number(itemQuantity));
+      //console.log(cartQuantity);
+      displayCart();
+    });
+  });
+
+  saveToStorage();
+  loadFromStorage();
+}
+
+
+//function to confirm an order
+export function confirmOrder() {
+  let orderPrice = 0;
+  let orderSummaryHTML = '';
+
+  cart.forEach((item) => {
+    const product = products.find(product => product.id === Number(item.id));
+    orderPrice += Number((product.price * item.quantity));
+    
+    //regenerate confirmed order through code structure of item order.
+    orderSummaryHTML += `
+      <div id="${item.id}">
+        <div class="cart-item-confirmed">
+          <div class="item-attribute-confirmed">
+            <img alt="${item.name}" src="${product.image.desktop}">
+        
+            <div class="item-info">
+              <span class="item-name">${item.name}</span>
+              <div style="display:flex; flex-direction: row">
+                <div class="item-quantity">${item.quantity}x</div>
+                <div class="item-price">@\$${(product.price).toFixed(2)}</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="total-price">
+            <b>$${(product.price * item.quantity).toFixed(2)}</b>
+          </div>
+        </div>
+      </div>
+
+      <div class="seperator-line"></div>
+    `;
+  });
+
+  let confirmOrderHTML = `
+    <div class="overlay"></div>
+    <div class="confirmed-order">
+      <img alt="confirmed-icon" src="assets/images/icon-order-confirmed.svg">
+      
+      <span style="font-size: 40px"><b>Order Confirmed</b></span>
+      
+      <span style="font-size: 16px; color: hsl(12, 20%, 44%); margin-top: 10px">
+        We hope you enjoy your food!
+      </span>
+
+      <div class="order-summary js-order-summary">
+        ${orderSummaryHTML}
+        <div class="order-price">
+          <span class="title">Order Total</span>
+          <span>
+            <h2>$${orderPrice.toFixed(2)}</h2>
+          </span>
+        </div>
+      </div>
+
+      <button class="start-new-order">
+        Start New Order
+      </button>
+    </div>
+  `;
+  // Append the confirmation HTML to the body or desired container
+  document.body.insertAdjacentHTML('beforeend', confirmOrderHTML);
+
+  //to make changing the color/overlay of the background after clicking on confirm order button
+  document.querySelector('.overlay').style.display = 'block';
+
+  //start a new order
+  const startNewOrderButton = document.querySelector('.start-new-order');
+  if (startNewOrderButton) {
+    startNewOrderButton.addEventListener('click', () => {
+      document.querySelector('.overlay').remove();
+      document.querySelector('.confirmed-order').remove();
+    });
+  }
+}
+
+// Load cart from storage on page load
+// document.addEventListener('DOMContentLoaded', () => {
+//   loadFromStorage();
+//   displayCart();
+// });
---
+++ ProductListWithCart/scripts2.js
@@ -0,0 +1,171 @@
+import { products } from "./products.js";
+import { 
+  addToCart, 
+  displayCart, 
+  removeFromCart, 
+  saveToStorage, 
+  loadFromStorage, 
+  confirmOrder 
+} from "./cart.js";
+
+// Code for generating HTML via JS
+const productMenu = document.querySelector('.js-menu-grid');
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+    <div class="item" id="${product.id}">
+      <div class="image-and-add">
+        <div class="item-image js-item-image">
+          <img alt="${product.name}" src="${product.image.desktop}">
+        </div>
+
+        <button class="add-button js-add-button" data-product-id="${product.id}">
+          <div class="add-to-cart js-add-to-cart" id="add">
+            <img class="bordered" alt="add-button-img" src="assets/images/icon-add-to-cart.svg">
+            <span class="add limit-text-to-2-lines">Add to Cart</span>
+          </div>
+        </button>
+      </div>
+      
+      <div class="info">
+        <div class="type">${product.category}</div>
+        <div class="name">${product.name}</div>
+        <div class="price">$${product.price.toFixed(2)}</div>
+      </div>
+    </div>
+  `
+  ;
+});
+productMenu.innerHTML = productsHTML;
+
+
+//starting for cart and saving datas
+export let cartQuantity = 0;
+
+export function updateCartQuantity() {
+  document.querySelector('.cart-quantity')
+    .innerText = `Your Cart (${cartQuantity})`;
+
+  displayCart();
+}
+
+//Add event listener:
+//function for each button
+function transformButtonToSelector(button) {
+  const buttonParent = button.parentElement;
+  const originalButtonHTML = buttonParent.innerHTML;
+  buttonParent.style.backgroundColor = 'hsl(14, 86%, 42%)';
+
+  // Update the button's parent element to include the quantity selector
+  buttonParent.innerHTML = `
+    <div class="quantity">
+      <button id="decrease" class="js-decrease-button">
+        <span>
+          <img src="assets/images/icon-decrement-quantity.svg">
+        </span>
+      </button>
+
+      <input type="text" value="1" name="input">
+      
+      <button id="increase" class="js-increase-button">
+        <span>
+          <img src="assets/images/icon-increment-quantity.svg">
+        </span>
+      </button>
+    </div>
+  `;
+  choosingQuantity(buttonParent, originalButtonHTML);
+  cartQuantity++; //why though? 
+  updateCartQuantity();
+}
+
+export function updateBorder(quantity, image) {
+  //changing border's color of the selected items:
+  //change border color if the quantity is more than 1 (item is selected).
+  if(quantity >= 1) {
+    image.style.borderColor = 'red';
+    image.style.borderWidth = '3px';
+    image.style.borderStyle = 'solid';
+    //console.log('error');
+  } else {
+    image.style.border = 'white';
+  }
+}
+
+
+export function choosingQuantity(buttonParent, originalButtonHTML) {
+  // Get the new elements
+  const quantitySelector = buttonParent.querySelector('.quantity');
+  const decreaseButton = quantitySelector.querySelector('.js-decrease-button');
+  const increaseButton = quantitySelector.querySelector('.js-increase-button');
+  const quantityDisplay = quantitySelector.querySelector('input');
+  const itemImage = buttonParent.closest('.item').querySelector('.js-item-image img');
+  //take item id to gather its data:
+  const itemId = buttonParent.closest('.item').id;
+
+  let quantity = parseInt(quantityDisplay.value, 10);  
+  
+  decreaseButton.addEventListener('click', (button) => {
+    if (quantity >= 1 || quantity === '') {
+      quantity--;
+      quantityDisplay.value = quantity;
+      //must be here to fulfill the condition
+      cartQuantity--;
+      updateCartQuantity();
+      removeFromCart(itemId, 1);
+      displayCart();
+      //call for update border after quantity over 1/ get selected.
+      updateBorder(quantity, itemImage); 
+    } 
+    else if(quantity === 0){
+      alert('You have no item to remove');
+      // Revert back to "Add to Cart" button
+      buttonParent.innerHTML = originalButtonHTML;
+      buttonParent.style.backgroundColor ='white';
+      
+      // Reattach event listener to the "Add to Cart" button
+      const addToCartButton = buttonParent.querySelector('.js-add-to-cart');
+      
+      addToCartButton.addEventListener('click', (event) => {
+        const buttonElement = event.currentTarget;
+        const productId = buttonElement.parentElement.dataset.productId;
+        transformButtonToSelector(buttonElement);
+        addToCart(productId, 1);
+      });
+    }
+  });
+
+  increaseButton.addEventListener('click', () => {
+    if (quantity < 50) {
+      quantity++;
+      quantityDisplay.value = quantity;
+      cartQuantity++;
+      //add to cart and item part
+      addToCart(itemId, 1);
+      updateCartQuantity();
+      displayCart();
+    } 
+    else {
+      alert('You have reached the limit of each order!');
+      quantityDisplay.value = 50;
+    }
+  });
+  updateBorder(1, itemImage);
+}
+
+// add an item to cart and add event listener into it
+document.addEventListener('DOMContentLoaded', () => {
+  const addToCartButtons = document.querySelectorAll('.js-add-to-cart');
+  addToCartButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const buttonElement = event.currentTarget;
+      const productId = button.parentElement.dataset.productId;
+      transformButtonToSelector(buttonElement);
+      addToCart(productId, 1);
+    });
+  });
+  updateCartQuantity();
+  displayCart();
+});
